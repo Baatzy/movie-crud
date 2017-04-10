57937cfb573dda71fb79f95c091bfe31e2035d80
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-10 11:22:31 -0700

    Updated landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Stefan\'s Hawt Movie Reviews' });
 });
 
 module.exports = router;
