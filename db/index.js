2a6a4a50956b228a04f479d414407da52b8b2f75
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-11 10:13:33 -0700

    Migrated table movies and seeded it

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
+'use strict'
+
 const env = 'development';
 const config = require('../knexfile.js')[env];
 const knex = require('knex')(config);
 
-const sql = knex('movies').toString();
-
-console.log(sql);
-
-knex.destroy();
+module.exports = knex
--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var db = require('../db')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('movies/index', { title: 'Movies List' });
-});
+router.get('/', (req, res, next) => {
+  db('movies').then(movies => {
+    res.render('movies/index', { movies })
+  })
+})
 
-module.exports = router;
+module.exports = router
