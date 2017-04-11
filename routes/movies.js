756d20fcce983927cf0c7c85ab6c174c49dac9c0
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-11 15:10:29 -0700

    Added new routes: -Added GET for one movie -Added form page to support CREATE new movie in database -Added CREATE for movies

--- routes/movies.js
+++ routes/movies.js
@@ -9,4 +9,35 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.get('/new', (req, res, next) => {
+  res.render('movies/new')
+})
+
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id
+  db('movies').select('*').where({ id: id }).first()
+  .then(movie => {
+    res.render('movies/details', { movie: movie })
+  })
+})
+
+router.post('/', (req, res, next) => {
+  let addedMovie = {
+    title: req.body.title,
+    director: req.body.director,
+    year: req.body.year,
+    my_rating: req.body['my-rating'],
+    poster_url: req.body['poster-url']
+  }
+  db('movies').insert(addedMovie, '*')
+  .then(newMovie => {
+    let id = newMovie[0].id
+
+    res.redirect(`/movies/${id}`)
+  })
+})
+
+
+
+
 module.exports = router
