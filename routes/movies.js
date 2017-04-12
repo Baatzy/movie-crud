f5b89e55716cc018713c844dce39e95c5b0837a0
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-12 15:29:01 -0700

    Got edit movie page to populate movie info

--- routes/movies.js
+++ routes/movies.js
@@ -2,26 +2,46 @@ var express = require('express')
 var router = express.Router()
 var db = require('../db')
 
-/* GET home page. */
+// GET route for all movie reviews
 router.get('/', (req, res, next) => {
+  // Grab all movies via an async request, then...
   db('movies').then(movies => {
+    // Pass the movies database object to Handlebars so it can render template
     res.render('movies/index', { movies })
   })
 })
 
+// GET route for new movie review form
 router.get('/new', (req, res, next) => {
+  // Render the form via Handlebars to add a new movie review
   res.render('movies/new')
 })
 
-router.get('/:id', (req, res, next) => {
+// PUT route to edit existing movie
+router.get('/:id/edit', (req, res, next) => {
+  // Grabbing req ID
   let id = req.params.id
-  db('movies').select('*').where({ id: id }).first()
-  .then(movie => {
+  // Grab all movies via an async request where the ID matches and take the first element of the result array, then...
+  db('movies').select('*').where({ id: id }).first().then(movie => {
+    // Pass the movie object to Handlebars so it can render template
+    res.render('movies/edit', { movie: movie })
+  })
+})
+
+// GET route for a movie by ID
+router.get('/:id', (req, res, next) => {
+  // Grabbing req ID
+  let id = req.params.id
+  // Grab all movies via an async request where the ID matches and take the first element of the result array, then...
+  db('movies').select('*').where({ id: id }).first().then(movie => {
+    // Pass the movie object to Handlebars so it can render template
     res.render('movies/details', { movie: movie })
   })
 })
 
+// POST route for new movie review
 router.post('/', (req, res, next) => {
+  // Taking the req.body from the form URL and assigning the form input field name data to keys in a new addedMovie object
   let addedMovie = {
     title: req.body.title,
     director: req.body.director,
@@ -29,15 +49,13 @@ router.post('/', (req, res, next) => {
     my_rating: req.body['my-rating'],
     poster_url: req.body['poster-url']
   }
-  db('movies').insert(addedMovie, '*')
-  .then(newMovie => {
+  // Making an async request to insert addedMovie, then...
+  db('movies').insert(addedMovie, '*').then(newMovie => {
+    // Grabbing the newMovie ID
     let id = newMovie[0].id
-
+    //Redirect the client to the new movie review when the new movie form is submitted
     res.redirect(`/movies/${id}`)
   })
 })
-
-
-
 
 module.exports = router
